5fb57c65a7c007c8c172dc5c43077793976febfa
Diego Iturra <diturra@dparadig.com>
2022-10-11 16:44:22 -0300

    change routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "books#index"
-  get "/books" ,to: "books#index"
-  get "/books/:id" ,to: "books#show"
-
+  resources :books
+  
 end
