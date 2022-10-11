60ab0e4da2778bc665a8a5f09ee49e3eb1de8221
Diego Iturra <diturra@dparadig.com>
2022-10-11 17:37:11 -0300

    include edit book option and back to index , refactor on forms to new/edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,9 +22,27 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit ,status: :unprocessable_entity
+    end
+  end
+
   private
   def book_params
-    params.require(:book).permit(:title,:author,:image)
+    params.require(:book).permit(:title,:description,:author,:image)
   end
 
 end
+
+#redirect_to: se usa cuando un objeto cambia su estado
+#render: se usa cuando un objeto no ha cmabiado su estado y no queremos
+#realizar nuevamente una peticion
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
   validates :title , presence: true
   validates :author , presence: true
   validates :image , presence: true
+  validates :description , presence: true
 end
