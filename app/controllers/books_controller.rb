81242e44b3a098df8b15b0576dc1dbb536c4eeb4
Diego Iturra <diturra@dparadig.com>
2022-10-11 17:12:05 -0300

    Add create new book and and validations to form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,23 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:author,:image)
+  end
+
 end
